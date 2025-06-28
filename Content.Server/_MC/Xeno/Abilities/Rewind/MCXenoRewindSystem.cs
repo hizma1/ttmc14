212ec6c7232b8be8eda3cc741a3e3f4be9cb0fb6
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2025-06-28 15:08:36 +1000

    feat: add rewind

---
+++ Content.Server/_MC/Xeno/Abilities/Rewind/MCXenoRewindSystem.cs
@@ -0,0 +1,21 @@
+﻿using Content.Server.Atmos.EntitySystems;
+using Content.Shared._MC.Xeno.Abilities.Rewind;
+using Content.Shared.Atmos.Components;
+
+namespace Content.Server._MC.Xeno.Abilities.Rewind;
+
+public sealed class MCXenoRewindSystem : MCSharedXenoRewindSystem
+{
+    [Dependency] private readonly FlammableSystem _flammable = default!;
+
+    protected override void OnTagetShutdown(Entity<MCXenoRewindTargetComponent> entity, ref ComponentShutdown _)
+    {
+        base.OnTagetShutdown(entity, ref _);
+
+        if (entity.Comp.Canceled)
+            return;
+
+        if (TryComp<FlammableComponent>(entity, out var flammableComponent))
+            _flammable.SetFireStacks(entity, entity.Comp.FireStacks, flammableComponent, entity.Comp.FireStacks > 0);
+    }
+}
