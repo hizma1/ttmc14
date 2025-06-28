6b93248c22b376d72de30dbad60be25350c21c50
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2025-06-28 23:50:47 +1000

    feat: wraith portal & sprite fix

---
+++ Content.Client/_MC/Xeno/Abilities/PortalPlacer/MCXenoChoosePortalMenu.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Content.Client.UserInterface.Controls;
+using Robust.Client.UserInterface.XAML;
+
+namespace Content.Client._MC.Xeno.Abilities.PortalPlacer;
+
+public sealed partial class MCXenoChoosePortalMenu : RadialMenu
+{
+    public MCXenoChoosePortalMenu()
+    {
+        RobustXamlLoader.Load(this);
+    }
+}
