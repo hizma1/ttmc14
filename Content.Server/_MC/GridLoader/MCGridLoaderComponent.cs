41d1ef590827fcb78fdc97c00fb14d639bef3412
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2025-06-30 13:36:25 +1000

    feat: alamo

---
+++ Content.Server/_MC/GridLoader/MCGridLoaderComponent.cs
@@ -0,0 +1,10 @@
+﻿using Robust.Shared.Utility;
+
+namespace Content.Server._MC.GridLoader;
+
+[RegisterComponent]
+public sealed partial class MCGridLoaderComponent : Component
+{
+    [DataField]
+    public ResPath Map;
+}
---
+++ Content.Server/_MC/GridLoader/MCGridLoaderSystem.cs
@@ -0,0 +1,26 @@
+﻿using Robust.Server.GameObjects;
+using Robust.Shared.EntitySerialization.Systems;
+
+namespace Content.Server._MC.GridLoader;
+
+public sealed class MCGridLoaderSystem : EntitySystem
+{
+    [Dependency] private readonly MapLoaderSystem _mapLoader = default!;
+    [Dependency] private readonly TransformSystem _transform = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<MCGridLoaderComponent, MapInitEvent>(OnMapInit);
+    }
+
+    private void OnMapInit(Entity<MCGridLoaderComponent> entity, ref MapInitEvent args)
+    {
+        var transform = Transform(entity);
+        var position = _transform.GetMapCoordinates(transform).Position;
+        var rotation = transform.LocalRotation;
+
+        _mapLoader.TryLoadGrid(transform.MapID, entity.Comp.Map, out _, offset: position, rot: rotation);
+    }
+}
