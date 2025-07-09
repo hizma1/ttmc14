8185f409061b8d4fad5baa4283f5f2ccee24538e
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2025-07-09 12:07:12 +1000

    feat: some changes

---
+++ Content.Shared/_MC/Armor/MCArmorGetEvent.cs
@@ -0,0 +1,16 @@
+﻿using Content.Shared.Inventory;
+
+namespace Content.Shared._MC.Armor;
+
+[ByRefEvent]
+public record struct  MCArmorGetEvent(
+    SlotFlags TargetSlots,
+    int Melee = 0,
+    int Bullet = 0,
+    int Laser = 0,
+    int Energy = 0,
+    int Bomb = 0,
+    int Bio = 0,
+    int Fire = 0,
+    int Acid = 0
+) : IInventoryRelayEvent;
--- Content.Shared/_MC/Armor/MCArmorSystem.cs
+++ Content.Shared/_MC/Armor/MCArmorSystem.cs
@@ -1,6 +1,8 @@
 ﻿using Content.Shared._MC.Xeno.Sunder;
+using Content.Shared._RMC14.Armor;
 using Content.Shared._RMC14.Weapons.Ranged;
 using Content.Shared.Damage;
+using Content.Shared.Inventory;
 using Content.Shared.Tag;
 using Content.Shared.Weapons.Melee;
 using Robust.Shared.Prototypes;
@@ -19,31 +21,69 @@ public sealed class MCArmorSystem : EntitySystem
     private static ProtoId<TagPrototype> TagAcid = "MCDamageAcid";
 
     [Dependency] private readonly TagSystem _tag = default!;
+    [Dependency] private readonly InventorySystem _inventory = default!;
     [Dependency] private readonly MCXenoSunderSystem _xenoSunder = default!;
 
     public override void Initialize()
     {
         base.Initialize();
 
+        SubscribeLocalEvent<MCArmorComponent, MCArmorGetEvent>(OnGet);
+        SubscribeLocalEvent<MCArmorComponent, InventoryRelayedEvent<MCArmorGetEvent>>(OnGetRelayed);
+
         SubscribeLocalEvent<MCArmorComponent, DamageModifyEvent>(OnDamageModify);
+
+        SubscribeLocalEvent<InventoryComponent, MCArmorGetEvent>(_inventory.RelayEvent);
+    }
+
+    private void OnGet(Entity<MCArmorComponent> entity, ref MCArmorGetEvent args)
+    {
+        args.Melee += entity.Comp.Melee;
+        args.Bullet += entity.Comp.Bullet;
+        args.Laser += entity.Comp.Laser;
+        args.Energy += entity.Comp.Energy;
+        args.Bomb += entity.Comp.Bomb;
+        args.Bio += entity.Comp.Bio;
+        args.Fire += entity.Comp.Fire;
+        args.Acid += entity.Comp.Acid;
+    }
+
+    private void OnGetRelayed(Entity<MCArmorComponent> entity, ref InventoryRelayedEvent<MCArmorGetEvent> args)
+    {
+        args.Args.Melee += entity.Comp.Melee;
+        args.Args.Bullet += entity.Comp.Bullet;
+        args.Args.Laser += entity.Comp.Laser;
+        args.Args.Energy += entity.Comp.Energy;
+        args.Args.Bomb += entity.Comp.Bomb;
+        args.Args.Bio += entity.Comp.Bio;
+        args.Args.Fire += entity.Comp.Fire;
+        args.Args.Acid += entity.Comp.Acid;
     }
 
     private void OnDamageModify(Entity<MCArmorComponent> entity, ref DamageModifyEvent args)
     {
-        var sunderModifier = _xenoSunder.GetSunder(entity.Owner);
+        DamageModify(entity, ref args);
+    }
+
+    private void DamageModify(EntityUid entityUid, ref DamageModifyEvent args)
+    {
+        var ev = new MCArmorGetEvent(SlotFlags.OUTERCLOTHING | SlotFlags.INNERCLOTHING);
+        RaiseLocalEvent(entityUid, ref ev);
+
+        var sunderModifier = _xenoSunder.GetSunder(entityUid);
 
         if (args.Tool is not { } tool)
             return;
 
         if (_tag.HasTag(tool, TagMelee) || HasComp<MeleeWeaponComponent>(tool))
         {
-            args.Damage *= ArmorToValue(entity.Comp.Melee, args.ArmorPiercing, sunderModifier);
+            args.Damage *= ArmorToValue(ev.Melee, args.ArmorPiercing, sunderModifier);
             return;
         }
 
         if (_tag.HasTag(tool, TagBullet) || HasComp<RMCBulletComponent>(tool))
         {
-            args.Damage *= ArmorToValue(entity.Comp.Bullet, args.ArmorPiercing, sunderModifier);
+            args.Damage *= ArmorToValue(ev.Bullet, args.ArmorPiercing, sunderModifier);
             return;
         }
     }
