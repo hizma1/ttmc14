3485e885d4e8eedcbe327b1ebab8644052bc2b01
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2025-07-12 04:17:33 +1000

    feat: update wraith actions

---
+++ Content.Shared/_MC/Xeno/Abilities/Banish/MCXenoBanishComponent.cs
@@ -0,0 +1,10 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._MC.Xeno.Abilities.Banish;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class MCXenoBanishComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public TimeSpan Duration;
+}
