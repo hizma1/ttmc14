04b848f076a2afa7d1c0394c3d161cb8ccd86044
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2025-07-14 02:21:39 +1000

    feat: banish

--- Content.Shared/_MC/Xeno/Abilities/Banish/MCXenoBanishComponent.cs
+++ Content.Shared/_MC/Xeno/Abilities/Banish/MCXenoBanishComponent.cs
@@ -5,6 +5,12 @@ namespace Content.Shared._MC.Xeno.Abilities.Banish;
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class MCXenoBanishComponent : Component
 {
+    [ViewVariables, AutoNetworkedField]
+    public EntityUid? Target;
+
     [DataField, AutoNetworkedField]
-    public TimeSpan Duration;
+    public TimeSpan Duration = TimeSpan.FromSeconds(10);
+
+    [DataField, AutoNetworkedField]
+    public float Range = 3;
 }
