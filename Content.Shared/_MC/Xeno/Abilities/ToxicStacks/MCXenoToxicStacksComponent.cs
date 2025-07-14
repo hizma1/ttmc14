df5315016ffc24529bb841c960dea5507062da98
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2025-07-15 00:25:21 +1000

    feat: spitter stacks done

--- Content.Shared/_MC/Xeno/Abilities/ToxicStacks/MCXenoToxicStacksComponent.cs
+++ Content.Shared/_MC/Xeno/Abilities/ToxicStacks/MCXenoToxicStacksComponent.cs
@@ -1,4 +1,6 @@
-﻿using Robust.Shared.GameStates;
+﻿using Content.Shared.Damage;
+using Robust.Shared.GameStates;
+using Robust.Shared.Serialization;
 
 namespace Content.Shared._MC.Xeno.Abilities.ToxicStacks;
 
@@ -6,5 +8,48 @@ namespace Content.Shared._MC.Xeno.Abilities.ToxicStacks;
 public sealed partial class MCXenoToxicStacksComponent : Component
 {
     [ViewVariables, AutoNetworkedField]
+    public TimeSpan NextTick;
+
+    [ViewVariables, AutoNetworkedField]
     public int Count;
+
+    [DataField, AutoNetworkedField]
+    public int Max = 30;
+
+    [DataField, AutoNetworkedField]
+    public DamageSpecifier BaseDamage = new()
+    {
+        DamageDict =
+        {
+            { "Burn", 1 }
+        },
+    };
+
+    [DataField, AutoNetworkedField]
+    public DamageSpecifier StacksDamage = new()
+    {
+        DamageDict =
+        {
+            { "Burn", 1 }
+        },
+    };
+
+    [DataField, AutoNetworkedField]
+    public TimeSpan TickInterval = TimeSpan.FromSeconds(2);
+
+    [DataField, AutoNetworkedField]
+    public int Decay = 1;
+}
+
+[Serializable, NetSerializable]
+public enum MCXenoToxicStacksLayer
+{
+    Base,
+    Icon,
+}
+
+[Serializable, NetSerializable]
+public enum MCXenoToxicStacksVisuals
+{
+    Visuals,
 }
--- Content.Shared/_MC/Xeno/Abilities/ToxicStacks/MCXenoToxicStacksSystem.cs
+++ Content.Shared/_MC/Xeno/Abilities/ToxicStacks/MCXenoToxicStacksSystem.cs
@@ -1,14 +1,57 @@
-﻿using Content.Shared.Projectiles;
+﻿using Content.Shared._RMC14.Slow;
+using Content.Shared.Damage;
+using Content.Shared.Mobs;
+using Content.Shared.Projectiles;
+using Robust.Shared.Timing;
 
 namespace Content.Shared._MC.Xeno.Abilities.ToxicStacks;
 
 public sealed class MCXenoToxicStacksSystem : EntitySystem
 {
+    [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
+    [Dependency] private readonly IGameTiming _timing = default!;
+    [Dependency] private readonly DamageableSystem _damageable = default!;
+    [Dependency] private readonly RMCSlowSystem _rmcSlow = default!;
+
     public override void Initialize()
     {
         base.Initialize();
 
+        SubscribeLocalEvent<MCXenoToxicStacksComponent, MobStateChangedEvent>(OnMobChangedState);
         SubscribeLocalEvent<MCXenoToxicStacksOnHitComponent, ProjectileHitEvent>(OnProjectileHit);
+    }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        var query = EntityQueryEnumerator<MCXenoToxicStacksComponent>();
+        while (query.MoveNext(out var uid, out var component))
+        {
+            if (_timing.CurTime < component.NextTick)
+                continue;
+
+            component.NextTick = _timing.CurTime + component.TickInterval;
+            Dirty(uid, component);
+
+            if (component.Count == 0)
+                continue;
+
+            _damageable.TryChangeDamage(uid, component.BaseDamage + component.StacksDamage * Math.Round(component.Count / 10f));
+
+            if (component.Count >= 20)
+                _rmcSlow.TrySlowdown(uid, TimeSpan.FromSeconds(1));
+
+            Add((uid, component), -component.Decay);
+        }
+    }
+
+    private void OnMobChangedState(Entity<MCXenoToxicStacksComponent> entity, ref MobStateChangedEvent args)
+    {
+        if (args.NewMobState != MobState.Dead)
+            return;
+
+        Set((entity, entity), 0);
     }
 
     private void OnProjectileHit(Entity<MCXenoToxicStacksOnHitComponent> entity, ref ProjectileHitEvent args)
@@ -29,7 +72,9 @@ public sealed class MCXenoToxicStacksSystem : EntitySystem
         if (!Resolve(entity, ref entity.Comp, logMissing: false))
             return;
 
-        entity.Comp.Count = count;
+        entity.Comp.Count = Math.Clamp(count, 0, entity.Comp.Max);
         Dirty(entity);
+
+        _appearance.SetData(entity, MCXenoToxicStacksVisuals.Visuals, entity.Comp.Count);
     }
 }
