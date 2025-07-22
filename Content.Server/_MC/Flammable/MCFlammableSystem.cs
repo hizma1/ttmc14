b44e3d80e147b8516c98a1ac2a9adb370ca2cf18
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2025-07-23 02:38:06 +1000

    feat: unrelenting force

---
+++ Content.Server/_MC/Flammable/MCFlammableSystem.cs
@@ -0,0 +1,15 @@
+﻿using Content.Server.Atmos.EntitySystems;
+using Content.Shared._MC.Flammable;
+
+namespace Content.Server._MC.Flammable;
+
+public sealed class MCFlammableSystem : MCSharedFlammableSystem
+{
+    [Dependency] private readonly FlammableSystem _flammable = default!;
+
+    public override void RemoveStacks(EntityUid uid, float stacks)
+    {
+        base.RemoveStacks(uid, stacks);
+        _flammable.AdjustFireStacks(uid, -stacks);
+    }
+}
---
+++ Content.Shared/_MC/Flammable/MCSharedFlammableSystem.cs
@@ -0,0 +1,8 @@
+﻿namespace Content.Shared._MC.Flammable;
+
+public abstract class MCSharedFlammableSystem : EntitySystem
+{
+    public virtual void RemoveStacks(EntityUid uid, float stacks)
+    {
+    }
+}
