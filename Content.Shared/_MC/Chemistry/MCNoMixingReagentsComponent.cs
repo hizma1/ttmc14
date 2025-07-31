7152c4c70a398a6e74282ac08bf69333769b82bf
F8ctor <ddcolored@gmail.com>
2025-07-31 07:11:49 -0300

    пошло нахуй

--- Content.Shared/_MC/Chemistry/MCNoMixingReagentsComponent.cs
+++
@@ -1,7 +0,0 @@
-﻿using Robust.Shared.GameStates;
-
-namespace Content.Shared._MC.Chemistry;
-
-[RegisterComponent, NetworkedComponent]
-[Access(typeof(SharedMCChemistrySystem))]
-public sealed partial class MCNoMixingReagentsComponent : Component;
