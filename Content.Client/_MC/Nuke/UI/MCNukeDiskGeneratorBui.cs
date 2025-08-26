b9ae07d807ca6bcb72e0ff1638571c76de84e451
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2025-08-26 23:34:20 +1000

    wip: nuke

---
+++ Content.Client/_MC/Nuke/UI/MCNukeDiskGeneratorBui.cs
@@ -0,0 +1,36 @@
+﻿using Content.Shared._MC.Nuke.UI;
+using Content.Shared.FixedPoint;
+using JetBrains.Annotations;
+using Robust.Client.UserInterface;
+
+namespace Content.Client._MC.Nuke.UI;
+
+[UsedImplicitly]
+public sealed class MCNukeDiskGeneratorBui : BoundUserInterface
+{
+    [ViewVariables]
+    private MCNukeDiskGeneratorWindow? _window;
+
+    public MCNukeDiskGeneratorBui(EntityUid owner, Enum uiKey) : base(owner, uiKey)
+    {
+    }
+
+    protected override void Open()
+    {
+        base.Open();
+
+        _window = this.CreateWindow<MCNukeDiskGeneratorWindow>();
+        _window.RunButton.OnPressed += _ => SendMessage(new MCNukeDiskGeneratorRunBuiMessage());
+
+        RefreshOverall(0);
+    }
+
+    private void RefreshOverall(FixedPoint2 value)
+    {
+        if (_window is null)
+            return;
+
+        _window.OverallProgressBar.Value = value.Float();
+        _window.OverallProgressLabel.Text = $"{value.Float()}%";
+    }
+}
---
+++ Content.Shared/_MC/Nuke/MCNukeDiskGeneratorSystem.cs
@@ -0,0 +1,23 @@
+﻿using Content.Shared._MC.Nuke.Events;
+using Content.Shared._MC.Nuke.UI;
+using Content.Shared.DoAfter;
+
+namespace Content.Shared._MC.Nuke;
+
+public sealed class MCNukeDiskGeneratorSystem : EntitySystem
+{
+    [Dependency] private readonly SharedDoAfterSystem _doAfter = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<MCNukeDiskGeneratorComponent, MCNukeDiskGeneratorRunBuiMessage>(OnRunMessage);
+    }
+
+    private void OnRunMessage(Entity<MCNukeDiskGeneratorComponent> entity, ref MCNukeDiskGeneratorRunBuiMessage args)
+    {
+        var doAfter = new DoAfterArgs(EntityManager, args.Actor, entity.Comp.InteractionTime, new MCNukeDiskGeneratorRunDoAfterEvent(), entity, entity, entity);
+        _doAfter.TryStartDoAfter(doAfter);
+    }
+}
